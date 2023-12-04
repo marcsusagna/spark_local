87f98dcfcaa2513b6088f172e8aae8f3b5c113f1
Marc Susagna <marcsusagna@gmail.com>
2023-12-04 09:12:28 +0100

    automate_data_extraction

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Called from root as 
+# bash scripts/get_data.sh
+
 mkdir data
 # Data can be found in http://ocelma.net/MusicRecommendationDataset/lastfm-1K.html
 cd data
 wget http://mtg.upf.edu/static/datasets/last.fm/lastfm-dataset-1K.tar.gz
-file_name=lastfm-dataset-1K.tar.gz
-tar xvf "$file_name"
+file_name=lastfm-dataset-1K
+extension=.tar.gz
+tar -xvf "${file_name}${extension}"
+
+mv $pwd$file_name/* .
+rm -r "$file_name"
